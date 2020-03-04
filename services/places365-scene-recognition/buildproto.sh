d368a6c5f2ae8c84a5b9d4b0c1d44885e8c55693
arturgontijo <arturgontijo@gmail.com>
2020-03-04 09:31:43 -0300

    [versions] Using latest versions of AI frameworks.

--- services/places365-scene-recognition/buildproto.sh
+++ services/places365-scene-recognition/buildproto.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/scene_recognition.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/scene_recognition.proto
