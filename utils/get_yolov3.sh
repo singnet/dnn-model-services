29ddbb7eeb833891579e28699dc0b1c557717a06
arturgontijo <arturgontijo@gmail.com>
2020-03-04 09:57:54 -0300

    [versions] Using latest versions of AI frameworks.

--- utils/get_yolov3.sh
+++ utils/get_yolov3.sh
@@ -15,7 +15,7 @@ if [ -f "${FILE}" ]
 then
 	echo "${FILE} found, skipping.."
 else
-	wget --no-check-certificate https://pjreddie.com/media/files/yolov3.weights
+	wget --no-check-certificate http://54.203.198.53:7000/PreTrainedDNNModels/Image/yolov3-object-detection/yolov3.weights
 fi
 
 FILE="yolov3.cfg"
@@ -23,5 +23,5 @@ if [ -f "${FILE}" ]
 then
 	echo "${FILE} found, skipping.."
 else
-	wget --no-check-certificate https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+	wget --no-check-certificate http://54.203.198.53:7000/PreTrainedDNNModels/Image/yolov3-object-detection/yolov3.cfg
 fi
